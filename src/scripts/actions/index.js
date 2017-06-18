49bb73e8b45464aed5647b9dc96a5909827745f4
james sewell <jamesjosephsewell@gmail.com>
2017-06-17 20:06:57 -0500

    updated url used for api

--- src/scripts/actions/index.js
+++ src/scripts/actions/index.js
@@ -8,7 +8,8 @@ import {
     LOADING_DATA,
     DATA_LOADED
 } from "./types"
-export const API_URL = "https://localhost:3000/api"
+//export const API_URL = "https://localhost:3000/api"
+export const API_URL = "https://website-starter.herokuapp.com/api"
 export const CLIENT_ROOT_URL = "http://localhost:8080"
 import { SubmissionError } from "redux-form"
 
