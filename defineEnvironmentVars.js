0b1a30becb65ed0fcb30fddabafd779e310dc65f
james sewell <jamesjosephsewell@gmail.com>
2017-07-05 23:05:06 -0500

    .

--- defineEnvironmentVars.js
+++
@@ -1,8 +0,0 @@
-exports.setEnvironmentVariables = function() {
-	process.env["FILESTACK_KEY"] = "AdJ0jZ9GGR6g4iEIZxIOwz"
-	process.env["NODEMAILER_CREDS"] = {
-		username: "nodemailjs@gmail.com",
-		pass: "thewebsite"
-	}
-	process.env["AUTH_SECRET"] = "placeholderstringlol"
-}
